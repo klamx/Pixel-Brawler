34c8fbc5fdc838ce6169150e17dc128ec9650420
Alejandro <largoalejo@gmail.com>
2020-05-21 17:09:53 -0500

    Pixel Brawler V0.1 Version donde se configura el movimiento y el ataque del jugador segun la direccion a la que este mirando

---
+++ Jugador.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Jugador(pygame.sprite.Sprite):
+	def __init__(self, posicion, m):
+		pygame.sprite.Sprite.__init__(self)
+		self.m = m
+		self.dir = 1
+		self.con = 0
+		self.accion = 0
+		self.image = self.m[self.accion][0]
+		self.rect = self.image.get_rect()
+		self.rect.x = posicion[0]
+		self.rect.y = posicion[1]
+		self.velx = 0
+		self.vely = 0
+
+		if self.accion == 0 or self.accion == 1 or self.accion == 2 or self.accion == 5 or self.accion == 6 or self.accion == 7:
+			self.limite = 3
+		elif self.accion == 3 or self.accion == 8:
+			self.limite = 5
+		elif self.accion == 4 or self.accion == 9:
+			self.limite = 1
+
+
+	def update(self):
+		self.rect.x += self.velx
+		self.rect.y += self.vely
+		
+		#if self.dir == 0:
+		#	self.accion = 5
+		#else:
+		#	self.accion = 0
+
+		if self.con < self.limite:
+			self.con += 1
+		else:
+			self.con = 0
+
+
+
+		self.image = self.m[self.accion][self.con]
--- Pixel_Brawler.py
+++ Pixel_Brawler.py
@@ -1,7 +1,7 @@
 import pygame
-
-ANCHO = 700
-ALTO = 500
+from Jugador import Jugador
+ANCHO = 500
+ALTO = 400
 NEGRO=[0,0,0]
 VERDE=[0,255,0]
 ROJO=[255,0,0]
@@ -39,10 +39,106 @@ if __name__ == '__main__':
 
 	# Seccion de configuracion del nivel
 	# Grupos
+	jugadores = pygame.sprite.Group()
 
+	# Imagenes
+	man_idle = pygame.image.load('Sprites/Man/Man_idle.png')
+	man_death = pygame.image.load('Sprites/Man/Man_death.png')
+	man_hurt = pygame.image.load('Sprites/Man/Man_hurt.png')
+	man_walk = pygame.image.load('Sprites/Man/Man_walk.png')
+	man_attack = pygame.image.load('Sprites/Man/Man_attack.png')
+	man_idle_left = pygame.image.load('Sprites/Man/Man_idle_left.png')
+	man_death_left = pygame.image.load('Sprites/Man/Man_death_left.png')
+	man_hurt_left = pygame.image.load('Sprites/Man/Man_hurt_left.png')
+	man_walk_left = pygame.image.load('Sprites/Man/Man_walk_left.png')
+	man_attack_letf = pygame.image.load('Sprites/Man/Man_attack_left.png')
 
-	fuente = pygame.font.Font(None, 40)
-	mensaje = fuente.render('Juego', True, BLANCO)
+	m = []
+	# accion 0 idle
+	for j in range(1):
+		fila = []
+		for c in range(4):
+			cuadro = man_idle.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 1 death
+	for j in range(1):
+		fila = []
+		for c in range(4):
+			cuadro = man_death.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 2 attack right
+	for j in range(1):
+		fila = []
+		for c in range(4):
+			cuadro = man_attack.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 3 walk left
+	for j in range(1):
+		fila = []
+		for c in range(6):
+			cuadro = man_walk.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 4 hurt
+	for j in range(1):
+		fila = []
+		for c in range(2):
+			cuadro = man_hurt.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 5 idle left
+	for j in range(1):
+		fila = []
+		for c in range(4):
+			cuadro = man_idle_left.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 6 death left
+	for j in range(1):
+		fila = []
+		for c in range(4):
+			cuadro = man_death_left.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 7 attack left
+	for j in range(1):
+		fila = []
+		for c in range(4):
+			cuadro = man_attack_letf.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 8 walk left
+	for j in range(1):
+		fila = []
+		for c in range(6):
+			cuadro = man_walk_left.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	# accion 9 hurt left
+	for j in range(1):
+		fila = []
+		for c in range(2):
+			cuadro = man_hurt_left.subsurface(48 * c, 48 * j, 48, 48)
+			fila.append(cuadro)
+		m.append(fila)
+
+	jugador = Jugador([100, 100], m)
+	jugadores.add(jugador)
+
+	# fuente = pygame.font.Font(None, 40)
+	# mensaje = fuente.render('Juego', True, BLANCO)
 	fin_juego = False
 	# Seccion de eventos
 	while (not fin) and (not fin_juego):
@@ -52,16 +148,46 @@ if __name__ == '__main__':
 				fin = True
 
 			if event.type == pygame.KEYDOWN:
-				print 'asd'
+				# Caminar hacia la derecha
+				if event.key == pygame.K_RIGHT:
+					jugador.dir = 1
+					jugador.accion = 3
+					jugador.velx = 5
 
+				# Caminar hacia la izquierda
+				if event.key == pygame.K_LEFT:
+					jugador.dir = 0
+					jugador.accion = 8
+					jugador.velx = -5
+
+				# Golpear 
+				if event.key == pygame.K_c:
+					# Mirando hacia la derecha
+					if jugador.dir == 1:
+						jugador.accion = 2
+					# Mirando hacia la izquierda
+					else:
+						jugador.accion = 7
+
+
+			if event.type == pygame.KEYUP:
+
+				if jugador.dir == 1:
+					jugador.accion = 0
+					jugador.velx = 0
+
+				else:
+					jugador.accion = 5
+					jugador.velx = 0
 
 		# Control
 
 		# Refresco
+		jugadores.update()
 		ventana.fill(NEGRO)
-		ventana.blit(mensaje, [450, 200])
+		jugadores.draw(ventana)
 		pygame.display.flip()
-		reloj.tick(15)
+		reloj.tick(13)
 
 	# Fin de juego
 	fuente = pygame.font.Font(None, 40)
